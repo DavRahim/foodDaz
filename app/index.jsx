f797065f42cd53f780212fd976382690de53e8c2
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-04-30 00:06:22 +0600

    fooddazm, it is an food delevery app in BD

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">index</Text>
+    </View>
+  );
+};
+
+export default index;
