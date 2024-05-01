bc935a37923ad829b9639f5ffd054a52c4556ebe
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-02 00:10:40 +0600

    home screen work going

--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const index = () => {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Home</Text>
     </View>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E21B70",
+      }
+    },
   },
   plugins: [],
 }
