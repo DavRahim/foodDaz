15de7320e9d9c39658e3c035374897b7cc559988
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-02 00:16:17 +0600

    home screen work going

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 const index = () => {
   return (
@@ -7,6 +9,10 @@ const index = () => {
       <View>
       <Text className="text-3xl ">Home</Text>
         <Text className="text-sm text-white">Rajia Fashion</Text>
+        <View className="ml-2">
+          <AntDesign name="hearto" size={24} color="black" />
+          <Feather name="shopping-bag" size={24} color="black" />
+        </View>
       </View>
     </View>
   );
