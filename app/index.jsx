52c0cd795535ad52cb79669e8fc0f164186bd7fd
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-02 00:12:30 +0600

    home screen work going

--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Home</Text>
+    <View className="flex-1 items-center justify-center bg-white ">
+      <View>
+      <Text className="text-3xl ">Home</Text>
+        <Text className="text-sm text-white">Rajia Fashion</Text>
+      </View>
     </View>
   );
 };
