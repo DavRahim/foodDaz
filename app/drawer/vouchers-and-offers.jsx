4d2db0e66be8370100836d2ca654872651bc719a
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-06 23:54:48 +0600

    voucher screen work stat

--- app/drawer/vouchers-and-offers.jsx
+++ app/drawer/vouchers-and-offers.jsx
@@ -1,11 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from '@expo/vector-icons';
 
 const VouchersAndOffers = () => {
   return (
     <SafeAreaView>
-      
+        <View>
+        <AntDesign name="arrowleft" size={24} color="black" />
+        <Text>Vouchers & Offers</Text>
+        </View>
     </SafeAreaView>
   );
 };
