cf2015b18853c596f1ff87ac6d08023e97d884a3
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-06 23:51:13 +0600

    voucher screen work stat

--- app/drawer/_layout.jsx
+++ app/drawer/_layout.jsx
@@ -1,16 +1,13 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { Drawer } from "expo-router/drawer";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Stack } from "expo-router";
 
 const DrawerLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="favorites" options={{headerShown: false}}/>
-      <Stack.Screen name="vouchers-and-offers"/>
       <Stack.Screen name="refund-account" options={{ headerShown: false }} />
-      <Stack.Screen name="become-pandapro" options={{ headerShown: false }} />
+      <Stack.Screen name="become-pandapro" options={{ headerShown: false }} />            
+      <Stack.Screen name="vouchers-and-offers" options={{ headerShown: false }} />            
     </Stack>
   );
 };
