92f626897afd03807451bfabef08da461034a0ca
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-06 23:52:09 +0600

    voucher screen work stat

--- app/drawer/vouchers-and-offers.jsx
+++ app/drawer/vouchers-and-offers.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const VouchersAndOffers = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>VouchersAndOffers</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
