0bc66d497bef58ac862240bd952377740b844e5f
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-07 00:20:28 +0600

    voucher screen work stat

--- app/drawer/vouchers-and-offers.jsx
+++ app/drawer/vouchers-and-offers.jsx
@@ -11,16 +11,16 @@ const VouchersAndOffers = () => {
         <Text className="text-[16px] font-bold">Vouchers & Offers</Text>
       </View>
       <ScrollView className="mx-[14px]">
-        <View className="w-[100%] flex flex-row justify-evenly items-center border ">
+        <View className="bg-white py-4 flex flex-row justify-evenly items-center border border-gray-200 rounded-xl mt-3 ">
 
-          <View className="w-[50%] mx-auto border-r">
-            <Text>Tk 0</Text>
+          <View className="border-r border-r-gray-200 pr-7">
+            <Text className="text-[17px] font-bold text-center">Tk 0</Text>
             <Text> saved this month</Text>
            </View>
 
-          <View className="w-[50%] mx-auto border-l">
-            <AntDesign name="creditcard" size={24} color="black" />
-            <Text>Add a Voucher</Text>
+          <View className="flex flex-row">
+            <AntDesign name="creditcard" size={24} color="#D00764" />
+            <Text className="text-[14px] font-bold ml-2 text-primary">Add a Voucher</Text>
            </View>
 
         </View>
