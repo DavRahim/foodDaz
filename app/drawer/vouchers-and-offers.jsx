f6abb712a1a872cdc1d6abcd52f0671d328dd105
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-07 00:02:36 +0600

    voucher screen work stat

--- app/drawer/vouchers-and-offers.jsx
+++ app/drawer/vouchers-and-offers.jsx
@@ -1,15 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign } from '@expo/vector-icons';
 
 const VouchersAndOffers = () => {
   return (
-    <SafeAreaView className="relative">
-        <View className="flex flex-row px-3 py-5 bg-white gap-5 border-b-gray-600 absolute top-0 left-0">
+    <SafeAreaView>
+      <View className="flex flex-row px-3 py-5 bg-white gap-5 border-b-gray-600">
         <AntDesign name="arrowleft" size={24} color="#D00764" />
         <Text className="text-[16px] font-bold">Vouchers & Offers</Text>
-        </View>
+      </View>
+      <ScrollView>
+
+      </ScrollView>
     </SafeAreaView>
   );
 };
