25f756cc77219bc6101f44760e1175f0eb6e794f
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-08 00:14:03 +0600

    favorites screen work on going

--- app/drawer/favorites.jsx
+++ app/drawer/favorites.jsx
@@ -1,16 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useNavigation } from "expo-router";
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Favorites = () => {
-    const navigation = useNavigation()
+
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <View className="flex gap-3">
-        <Text onPress={() => navigation.openDrawer()} className="text-primary">{}Food Daz</Text>
-      <Text>Favorites</Text>
-      </View>
+    <SafeAreaView className="">
+      <ScrollView className="mx-[14px]">
+        {/* header */}
+        <View className="flex-row justify-between items-center py-3">
+          <View className="flex-row items-center">
+            <AntDesign name="arrowleft" size={24} color="#D00764" />
+          <Text className="ml-5 text-[18px] font-bold">Favorites</Text>
+          </View>
+          <View className="relative">
+            <Ionicons name="bag-handle-outline" size={24} color="#D00764" />
+          </View>
+          <Text className="absolute top-6 right-0 bg-primary px-[5px] text-[12px] rounded-full text-white font-bold">2</Text>
+        </View>
+
+        {/* header */}
+
+        <View className="mt-6 border-b border-b-[#3333]">
+          <View className="flex-row mx-12 justify-between items-center pb-3">
+            <Text className="text-[16px] font-bold text-primary">Restaurants</Text>
+            <Text className="text-[16px] font-bold">shops</Text>
+          </View>
+          <View className="border-2 bg-primary border-primary rounded-2xl w-[120px] ml-8 "></View>
+
+
+        </View>
+
+      </ScrollView>
+      
     </SafeAreaView>
   );
 };
