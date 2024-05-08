fbbdde9a658b034e0454ac05f00bc32c65dd1689
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-08 23:53:10 +0600

    panda rewards screen work start

---
+++ app/drawer/panda-rewards.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PandaRewards = () => {
+  return (
+    <View>
+      <Text>PandaRewards</Text>
+    </View>
+  );
+};
+
+export default PandaRewards;
