71052d2041da78718bef79fb89c73436eec148bd
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-10 00:04:16 +0600

    invite friends screen work ongoing

--- app/drawer/invite-friends.jsx
+++ app/drawer/invite-friends.jsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StatusBar, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from '@expo/vector-icons';
+import inv from "../../assets/rew/rew6.png"
 
 const InviteFriends = () => {
   return (
-    <View>
-      <Text>InviteFriends</Text>
-    </View>
+    <SafeAreaView className="bg-white">
+          <View className="bg-white px-4 flex-row items-center py-4">
+              <AntDesign name="arrowleft" size={30} color="#D00764" />
+              <Text className="text-[18px] font-bold ml-4">How it works</Text>
+          </View>
+          <View className="bg-white mx-auto">
+            <Image
+            source={inv}
+             className="w-[200px] h-[200px] mx-auto"
+             resizeMode="cover"
+            />
+            <Text className="w-[250px] text-center text-[20px] font-bold">
+                Invite your friends for a TK 200 referral bonus!
+            </Text>
+
+          </View>
+
+          <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"}/>
+    </SafeAreaView>
   );
 };
 
