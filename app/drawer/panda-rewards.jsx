fe30bc69319c65c4ed17bc2fb2309b110704e5df
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-09 23:05:38 +0600

    panda rewards screen work on going

--- app/drawer/panda-rewards.jsx
+++ app/drawer/panda-rewards.jsx
@@ -1,45 +1,201 @@
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image, StatusBar, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome, AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import rew from "../../assets/rew/rew.png"
+import rew1 from "../../assets/rew/rew1.png"
 
 const PandaRewards = () => {
-  return (
-    <SafeAreaView>
-        <View className="bg-primary pb-32 relative">
-        <View className="flex-row justify-between items-center p-3">
-            <View className="w-[30px] h-[30px] bg-white rounded-full py-1 px-1">
-                      <AntDesign name="arrowleft" size={20} color="#D00764" />
-            </View>
-                  <View className="w-[30px] h-[30px] bg-white rounded-full py-1 pl-[6]">
-                      <FontAwesome name="question-circle-o" size={20} color="#D00764" />
-            </View>
-        </View>
-        <Image
-          source={rew}
-          className="mx-auto"
-         />
-          </View>
-          <View className="absolute bottom-4 left-3 w-[95%] bg-white pb-4 pt-5 rounded-2xl">
-            <Text className="text-center text-[15px] font-semibold">
-                Points
-            </Text>
-            <Text className="text-center mt-2">
-                  <MaterialCommunityIcons name="star-circle-outline" size={24} color="black" />
-                  <Text className="text-[25px] font-bold">0</Text>
-                  <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
-            </Text>
-            <Text className="text-center text-[16px] font-bold text-primary mt-4">
-                How to earn points
-            </Text>
-          </View>
-          <StatusBar backgroundColor={"#D00764"} barStyle={"light-content"}/>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView className="">
+            <ScrollView>
+                <View className="bg-primary pb-36 relative">
+                    <View className="flex-row justify-between items-center p-3">
+                        <View className="w-[30px] h-[30px] bg-white rounded-full py-1 px-1">
+                            <AntDesign name="arrowleft" size={20} color="#D00764" />
+                        </View>
+                        <View className="w-[30px] h-[30px] bg-white rounded-full py-1 pl-[6]">
+                            <FontAwesome name="question-circle-o" size={20} color="#D00764" />
+                        </View>
+                    </View>
+                    <Image
+                        source={rew}
+                        className="mx-auto"
+                    />
+                    <View className="absolute bottom-6 left-3 w-[95%] bg-white pb-4 pt-5 rounded-2xl">
+                        <Text className="text-center text-[15px] font-semibold">
+                            Points
+                        </Text>
+                        <Text className="text-center mt-2">
+                            <MaterialCommunityIcons name="star-circle-outline" size={24} color="black" />
+                            <Text className="text-[25px] font-bold">0</Text>
+                            <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+                        </Text>
+                        <Text className="text-center text-[16px] font-bold text-primary mt-4">
+                            How to earn points
+                        </Text>
+                    </View>
+                </View>
+
+
+                {/* cart section */}
+                <View className="bg-white -mt-3 rounded-t-2xl">
+                    <Text className="my-6 text-[18px] font-bold mx-4">Complete stamp cards to win</Text>
+
+                    <ScrollView horizontal={true} className="pl-6">
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View className="w-[280px] rounded-2xl border border-[#3333331e] bg-slate-100 p-4 ml-3">
+                            <Text className="">Food Delivery Order(10X)</Text>
+                            <Text className="text-[16px] font-bold mt-2">CelebrEAT Mothers Day with five star dinner for two!</Text>
+                            <Image
+                                source={rew1}
+                                className="w-[120px] h-[120px] ml-auto" 
+                            />
+                            <View className="flex-row justify-between items-start mx-3 mt-5">
+                                <View className="flex-row justify-center items-center">
+                                    <FontAwesome5 name="bitcoin" size={24} color="black" />
+                                     <View className="ml-3">
+                                        <Text className="text-[14px] font-bold">To order</Text>
+                                        <Text className="text-[14px] font-bold">10 order</Text>
+                                     </View>
+                                </View>
+                                <View className="border border-primary rounded-lg">
+                                    <Text className="text-[15px] text-primary font-bold py-1 px-3">Start</Text>
+                                </View>
+                            </View>
+                        </View>
+
+                    </ScrollView>
+
+
+                </View>
+                <StatusBar backgroundColor={"#D00764"} barStyle={"light-content"} />
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default PandaRewards;
